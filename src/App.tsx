49fa12c6ea321a36b622651cfa2fea2b9574d9e0
Rich McGhee <rmcghee@stowers.org>
2024-04-01 14:11:47 -0500

    Added delay to grow render.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Container, ThemeProvider } from '@mui/material';
 import { CssBaseline } from '@mui/material';
 import { Box } from '@mui/system';
@@ -14,6 +14,22 @@ import { LeftGrow } from './common/Basic';
 import BottomNav from './BottomNav';
 
 function App() {
+  const [renderOrder, setRenderOrder] = useState(0);
+  const [renderTimer, setRenderTimer] = useState<NodeJS.Timer|null>(null);
+
+  useEffect(() => {
+      const interval = setInterval(() => setRenderOrder(renderOrder + 1), 450);
+      setRenderTimer(interval);
+
+      return () => clearInterval(interval);
+  }, [renderOrder]);
+
+  useEffect(() => {
+    if (renderOrder >= 4 && renderTimer !== null) {
+      clearInterval(renderTimer);
+    }
+  }, [renderOrder, renderTimer]);
+
   return (
     <ThemeProvider theme={theme}>
     <CssBaseline>
@@ -21,13 +37,22 @@ function App() {
       <LeftGrow><Box sx={{ flexGrow: 0}} style={{ marginTop: 15 }}>
         <h1>portfolio</h1>
       </Box></LeftGrow>
+      
       <Box sx={{
         flexGrow: 1,
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'space-between',
         maxWidth: '500px',
       }}>
+        <LeftGrow trigger={renderOrder > 0}><Box sx={{ flexGrow: 0}} style={{ marginTop: 15 }}>
+          <h3>joule-home</h3>
+        </Box></LeftGrow>
+        <LeftGrow trigger={renderOrder > 1}><Box sx={{ flexGrow: 0}} style={{ marginTop: 15 }}>
+          <h3>photography</h3>
+        </Box></LeftGrow>
+        <LeftGrow trigger={renderOrder > 2}><Box sx={{ flexGrow: 0}} style={{ marginTop: 15 }}>
+          <h3>synthetic biology</h3>
+        </Box></LeftGrow>
       </Box>
       <BottomNav/>
     </Container>
--- src/common/Basic.tsx
+++ src/common/Basic.tsx
@@ -1,12 +1,7 @@
-import { Grow, TextField, TextFieldProps, Tooltip } from '@mui/material';
-import React, { useState, ReactElement } from 'react';
+import { Grow } from '@mui/material';
+import React, { useState, ReactElement, useEffect } from 'react';
 
 const prod_env = false;
-export const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-export const btuInkWh = 3412;
-export const btuInCcf = 103900;
-export const copInSeer = 0.293;
-export const supabaseBaseUrl = prod_env ? 'https://uqjgvhebgvzrbbfjcxsg.supabase.co/functions/v1/' : 'http://127.0.0.1:54321/functions/v1/';
 
 interface LeftGrowProps {
     children: ReactElement;
@@ -19,5 +14,5 @@ interface LeftGrowProps {
     <Grow in={trigger} style={{ transformOrigin: '-30% 50%' }} timeout={timeout}>
       {children}
     </Grow>
-  );
+    );
 };
