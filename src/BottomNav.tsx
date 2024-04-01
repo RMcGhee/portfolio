39800979ec3a40be9be1439b7559842c7e697e15
Rich McGhee <rmcghee@stowers.org>
2024-04-01 14:16:20 -0500

    Removed tooltip from bottom nav

--- src/BottomNav.tsx
+++ src/BottomNav.tsx
@@ -12,15 +12,6 @@ import '@fontsource/roboto/700.css';
 const user_home_url = 'https://rmcghee.github.io/'
 
 const BottomNav: React.FC = () => {
-  const tooltipContent = (
-    <React.Fragment>
-      <Typography color="inherit">Additional Info</Typography>
-      <Link href={user_home_url + "extra_info.html"} target="_blank" rel="noopener noreferrer">
-        Click here for more info
-      </Link>
-    </React.Fragment>
-  );
-
   return (
     <LeftGrow>
       <Stack
@@ -29,13 +20,11 @@ const BottomNav: React.FC = () => {
         alignItems={'flex-end'}
         justifyContent={'flex-start'}
       >
-        <Tooltip title={tooltipContent} enterDelay={0}>
-          <h2 style={{ cursor: 'pointer' }}>r.mcghee</h2>
-        </Tooltip>
+        <h2>r.mcghee</h2>
         <Divider orientation='vertical'/>
-        <Link href={user_home_url + "synbio.html"}>synthetic biology</Link><br />
+        <Link href="/joule-home">joule-home</Link><br />
         <Link href={user_home_url + "photography.html"}>photography</Link><br />
-        <Link href="/">joule-home</Link><br />
+        <Link href={user_home_url + "synbio.html"}>synthetic biology</Link><br />
       </Stack>
     </LeftGrow>
   );
