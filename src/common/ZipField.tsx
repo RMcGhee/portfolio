7a4677b7eb2726fcd47c1be9fb95dc47abe809df
Rich McGhee <rmcghee@stowers.org>
2024-11-10 06:46:56 -0600

    Debug

--- src/common/ZipField.tsx
+++ src/common/ZipField.tsx
@@ -44,6 +44,7 @@ export const ZipField: React.FC<ZipFieldProps> = ({
         });
         // if (!response.ok) throw new Error('Network response was not ok');s
         const responseData = await response.json();
+        console.log(responseData)
         zips = responseData.data[0] as ZipDist;
         console.log(zips)
       // } catch (e) {
